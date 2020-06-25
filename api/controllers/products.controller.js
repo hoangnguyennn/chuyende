dd489f232d23dd03ad8718970027c20109daa9ca
hoangnguyen <nvanhoang188@gmail.com>
2020-06-25 20:00:55 +0700

    add filter for search products

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -1,10 +1,49 @@
 const ProductBO = require("../../models/bo/products.bo");
 const nanoid = require("../../utils/nanoid");
+const { removeAccents } = require("../../utils/formatter");
 const logger = require("../../utils/logger");
 
 module.exports.index = async (req, res) => {
+  const { limit, offset, all, q, minPrice, maxPrice, order } = req.query;
+
   try {
     let products = await ProductBO.getAll();
+
+    if (!all) {
+      products = products.filter((product) => product.status === "Selling");
+    }
+
+    if (q) {
+      products = products.filter((product) => {
+        return removeAccents(product.name.toLowerCase()).includes(
+          removeAccents(q.toLowerCase())
+        );
+      });
+    }
+
+    if (minPrice) {
+      products = products.filter((product) => product.price >= minPrice);
+    }
+
+    if (maxPrice) {
+      products = products.filter((product) => product.price <= maxPrice);
+    }
+
+    if (order) {
+      if (order === "asc") {
+        products.sort((a, b) => a.price - b.price);
+      } else if (order === "desc") {
+        products.sort((a, b) => b.price - a.price);
+      }
+    }
+
+    if (limit) {
+      if (offset) {
+        products = products.slice(+offset, +offset + +limit);
+      } else {
+        products = products.slice(0, +limit);
+      }
+    }
 
     return res.status(200).json({
       data: products,
